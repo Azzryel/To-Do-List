71519e575a018014502a5a0139b79029f203c623
Azzryel <AzryelShroud@gmail.com>
2020-01-04 20:58:13 +0100

    add template and display for toDo tasks, right container header, some changes

--- src/createTemplate.js
+++ src/createTemplate.js
@@ -1,3 +1,5 @@
+import { projectCollection } from "./projectFactory";
+
 const createTemplate = ( () => {
 
     const projectTemplate = (e, place) => {
@@ -6,13 +8,59 @@ const createTemplate = ( () => {
         projectElement.className = "projects";
         projectElement.setAttribute("data-place", `${place}`);
         const para = document.createElement("p");
+        para.setAttribute("data-place", `${place}`);
         para.textContent = `${e.title}`
 
         projectElement.appendChild(para);
         projectList.appendChild(projectElement);
     }
 
-    return { projectTemplate }
+    const toDoTemplate = (e) => {
+        const toDoList = document.querySelector("#to-do-list");
+        const toDoContent = document.createElement("div");
+        toDoContent.className = "to-do-content";
+        const toDoCheck = document.createElement("input");
+        toDoCheck.setAttribute("type", "checkbox");
+        toDoCheck.className = "checkbox";
+        const span = document.createElement("span");
+        span.textContent = `${e.title}`;
+
+        
+        toDoList.appendChild(toDoContent);
+        toDoContent.appendChild(toDoCheck);
+        toDoContent.appendChild(span);
+    }
+
+    const projectTitleHeader = (place) => {
+        const projectTitle = document.querySelector("#project-title-header");
+        const projectHeader = document.createElement("div");
+        projectHeader.id = "header-title";
+        const titleSpan = document.createElement("span");
+        titleSpan.textContent = `${projectCollection[place].title}`;
+        const projectRemove =document.createElement("button");
+        projectRemove.className = "remove-projects";
+        projectRemove.id = `remove-project-${place}`;
+        projectRemove.setAttribute("data-remove", `${place}`);
+        projectRemove.textContent = "Remove project";
+
+
+        projectTitle.appendChild(projectHeader);
+        projectHeader.appendChild(titleSpan);
+        projectHeader.appendChild(projectRemove);
+    }
+
+    const projectDefaultHeader = () => {
+        const projectTitle = document.querySelector("#project-title-header");
+        const projectHeader = document.createElement("div");
+        projectHeader.id = "header-title";
+        const titleSpan = document.createElement("span");
+        titleSpan.textContent = `Default Poject Title`;
+
+        projectTitle.appendChild(projectHeader);
+        projectHeader.appendChild(titleSpan);
+    }
+
+    return { projectTemplate, toDoTemplate, projectTitleHeader , projectDefaultHeader }
 })();
 
 export { createTemplate }
--- src/display.js
+++ src/display.js
@@ -10,7 +10,45 @@ const display = ( () => {
         });
     }
 
-    return { render }
+    const renderProjectHeader = (e) => {
+        createTemplate.projectTitleHeader(e);
+    }
+
+    const renderToDo = (place) => {
+        projectCollection[place].toDoTasks.forEach( (e) => {
+            createTemplate.toDoTemplate(e);
+        })
+    }
+
+    const renderLastProject = (project, place) => {
+        createTemplate.projectTemplate(project, place);
+    }
+
+    const renderLastToDo = (toDo) => {
+        createTemplate.toDoTemplate(toDo)
+    }
+
+    const remove = () => {
+        const elements = document.getElementsByClassName("to-do-content");
+        while(elements.length > 0){
+            elements[0].parentNode.removeChild(elements[0]);
+        }
+
+        const element = document.getElementById("header-title");
+        element.parentNode.removeChild(element);
+    }
+
+    const removeProject = (place) => {
+        console.log("hi")
+        const element = document.querySelector(`[data-place = "${place}"]`);
+        element.parentNode.removeChild(element);
+    }
+
+    const renderDefaultHeader = () => {
+        createTemplate.projectDefaultHeader();
+    }
+
+    return { render, renderProjectHeader, renderToDo, renderLastProject, renderLastToDo, renderDefaultHeader, remove, removeProject }
 })();
 
 export { display }
